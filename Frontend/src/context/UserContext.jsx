875f4c94031cda274d50b4d0531ccef63f16a2f1
Shrayansh <shrayanshgoinka@gmail.com>
2025-07-12 01:13:34 +0530

    made userContext

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { use } from 'react'
+import { createContext, useState } from 'react';
+
+export const UserDataContext = createContext();
 
 const UserContext = ({children}) => {
+
+  const[user, setUser] =  useState({
+    email : '',
+    fullName :{
+      firstName : '',
+      lastName : ''
+    }
+  });
+
   return (
     <div>
-      {children}
+      <UserDataContext.Provider value={[user, setUser]}>
+        {children}
+      </UserDataContext.Provider>
     </div>
   )
 }
